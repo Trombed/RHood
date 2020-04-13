16d8828867e44f516de3baa06df2fd64b635725d
Eric Ho <eric.h924@gmail.com>
2020-04-13 02:24:26 -0700

    fixing heroku db

--- db/migrate/20200407002920_edit_transaction.rb
+++ db/migrate/20200407002920_edit_transaction.rb
@@ -1,6 +1,10 @@
 class EditTransaction < ActiveRecord::Migration[5.2]
   def change
     remove_column :transactions, :shares
+    remove_column :transactions, :shares_bought
+    remove_column :transactions, :shares_buy_price
+    remove_column :transactions, :shares_sell_price
+    remove_column :transactions, :is_complete
     add_column :transactions, :shares, :integer, :default => 0
   end
 end
