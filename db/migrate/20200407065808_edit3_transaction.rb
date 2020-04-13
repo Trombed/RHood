729be884a7a425d942975d6fdbf0fbb1a5f9e8f6
Eric Ho <eric.h924@gmail.com>
2020-04-13 02:12:43 -0700

    fixing heroku db

--- db/migrate/20200407065808_edit3_transaction.rb
+++ db/migrate/20200407065808_edit3_transaction.rb
@@ -1,6 +1,6 @@
 class Edit3Transaction < ActiveRecord::Migration[5.2]
   def change
-    remove_column :transactions, :average_price
+ 
     add_column :transactions, :total_price, :integer, :default => 0
   end
 end
