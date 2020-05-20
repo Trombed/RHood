53a937b2617b06e2190043e7a99eca75ade83b67
Eric Ho <eric.h924@gmail.com>
2020-05-19 22:52:48 -0700

    add 404 page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
   namespace :api, defaults: {format: :json} do 
     resources :users, only: [:create,:show] 
     resource :session, only: [:create, :destroy]
@@ -11,4 +13,7 @@ Rails.application.routes.draw do
   
 
   root to: "static_pages#root"
+  RHood::Application.routes.draw do
+    get "*any", via: :all, to: "errors#not_found"
+  end
 end
